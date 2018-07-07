27909842f0291c776942d5f1a808c7051809eaaa
ikhwanuddin <rifqi@ikhwanudd.in>
2018-07-07 17:16:16 +0700

    a

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd static
+sudo rm -r *
+sudo rm .DS_Store
+ls -la
+git commit -m "Bersihkan sudo rm -r *"
+cd ..
+sudo ./buster.py generate --domain=http://localhost:2368
+sudo cp CNAME static/
+sudo cp README.md static/
+sudo cp deploy.sh static/
+ls -la
+sudo ./buster.py deploy
