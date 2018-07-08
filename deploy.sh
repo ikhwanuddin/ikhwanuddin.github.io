1eb30c5161465b9c965c0f1d380238e00706a666
ikhwanuddin <rifqi@ikhwanudd.in>
2018-07-09 00:15:25 +0700

    @ikhwanudd.in diperbarui pada 2018-07-08 17:15:25

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,11 @@
 cd static
 sudo rm -r *
 sudo rm .DS_Store
-ls -la
 git commit -m "Bersihkan sudo rm -r *"
-git push 
+git push
 cd ..
 sudo ./buster.py generate --domain=http://localhost:2368
 sudo cp CNAME static/
 sudo cp README.md static/
 sudo cp deploy.sh static/
-ls -la
 sudo ./buster.py deploy
