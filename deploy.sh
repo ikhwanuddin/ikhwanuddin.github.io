15ec20f09a3941c8fcdd8da05e15e4f84e98fa47
ikhwanuddin <rifqi@ikhwanudd.in>
2018-09-09 23:20:01 +0700

    @ikhwanudd.in diperbarui pada 2018-09-09 16:20:00

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ sudo ./buster.py generate --domain=http://localhost:2368
 sudo cp CNAME static/
 sudo cp README.md static/
 sudo cp deploy.sh static/
+sudo cp googleadbed33964f3bd03.html static/
 sudo ./buster.py deploy
