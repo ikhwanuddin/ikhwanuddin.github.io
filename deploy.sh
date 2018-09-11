2afa68944b966a3889028aeb6fdc9eeed703fb2a
ikhwanuddin <rifqi@ikhwanudd.in>
2018-09-12 00:57:02 +0700

    @ikhwanudd.in diperbarui pada 2018-09-11 17:57:02

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,11 @@ sudo rm -r *
 sudo rm .DS_Store
 git commit -m "Bersihkan sudo rm -r *"
 git push
-mkdir rss
 cd ..
 sudo ./buster.py generate --domain=http://localhost:2368/
 wget http://127.0.0.1:2368/rss
-sudo mv rss index.xml
-sudo cp index.xml static/rss/
+sudo mv rss rss.xml
+sudo cp rss.xml static/
 sudo cp CNAME static/
 sudo cp README.md static/
 sudo cp deploy.sh static/
